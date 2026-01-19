edf900d98f37adf3c02367683c7880ddc94e49bf
Sleeeee <jstasjustin@gmail.com>
2026-01-19 11:34:23 +0100

    DVH #18 Enforce stricter (unique) naming convention and move flags to global scope to allow ELF symbol reading for Renode

--- firmware/labs/00_swd/lab_swd.c
+++ firmware/labs/00_swd/lab_swd.c
@@ -1,13 +1,21 @@
 #include "lab_swd.h"
 #include <stdio.h>
 
-static void Solve_Flag_One(const char* flag, volatile char* buffer) {
+static const char LAB_SWD_FLAG_ONE[] = "DVH{pl41n_text_d3t3ct1v3_fb242a8175}";
+
+// DVH{h1d1ng_b3h1nd_x0r_4e2f398b6b}
+static const unsigned char LAB_SWD_FLAG_TWO[] = {0xef, 0xfd, 0xe3, 0xd0, 0xc3, 0x9a, 0xcf, 0x9a, 0xc5, 0xcc, 0xf4, 0xc9, 0x98, 0xc3, 0x9a, 0xc5, 0xcf, 0xf4, 0xd3, 0x9b, 0xd9, 0xf4, 0x9f, 0xce, 0x99, 0xcd, 0x98, 0x92, 0x93, 0xc9, 0x9d, 0xc9, 0xd6};
+
+// DVH{y0ur_b1n_1s_l34k1ng_c8ffc55f5e}
+static const unsigned char LAB_SWD_FLAG_THREE[] = {0xe8, 0xfa, 0xec, 0x1f, 0x1d, 0xd4, 0x19, 0x16, 0x03, 0x06, 0xd5, 0xf2, 0x03, 0xd5, 0x17, 0x03, 0xf0, 0xd7, 0xd8, 0x0f, 0xd5, 0xf2, 0x0b, 0x03, 0x07, 0xdc, 0x0a, 0x0a, 0x07, 0xd9, 0xd9, 0x0a, 0xd9, 0x09, 0x01, 0xa4};
+
+static void Lab_SWD_Solve_Flag_One(const char* flag, volatile char* buffer) {
   // Print the flag into the buffer
   // Casting from volatile char* so sprintf understands
   sprintf((char*)buffer, "%s", flag);
 }
 
-static void Solve_Flag_Two(const unsigned char* flag, int len, volatile char* buffer) {
+static void Lab_SWD_Solve_Flag_Two(const unsigned char* flag, int len, volatile char* buffer) {
   // XOR the encrypted flag bytes using the key
   const unsigned char XOR_KEY = 0xab;
 
@@ -17,7 +25,7 @@ static void Solve_Flag_Two(const unsigned char* flag, int len, volatile char* bu
   buffer[len] = '\0';
 }
 
-static void Solve_Flag_Three(const unsigned char* flag, int len, volatile char* buffer) {
+static void Lab_SWD_Solve_Flag_Three(const unsigned char* flag, int len, volatile char* buffer) {
   // Combination of XOR and single-byte arithmetics to print the flag
   const unsigned char INCREMENT = 0x31;
   const unsigned char XOR_KEY = 0x0f;
@@ -29,19 +37,11 @@ static void Solve_Flag_Three(const unsigned char* flag, int len, volatile char* 
 }
 
 void Lab_SWD_Init(void) {
-  const char FLAG_ONE[] = "DVH{pl41n_text_d3t3ct1v3_fb242a8175}";
+  volatile char lab_swd_buffer[64];
 
-  // DVH{h1d1ng_b3h1nd_x0r_4e2f398b6b}
-  const unsigned char FLAG_TWO[] = {0xef, 0xfd, 0xe3, 0xd0, 0xc3, 0x9a, 0xcf, 0x9a, 0xc5, 0xcc, 0xf4, 0xc9, 0x98, 0xc3, 0x9a, 0xc5, 0xcf, 0xf4, 0xd3, 0x9b, 0xd9, 0xf4, 0x9f, 0xce, 0x99, 0xcd, 0x98, 0x92, 0x93, 0xc9, 0x9d, 0xc9, 0xd6};
-
-  // DVH{y0ur_b1n_1s_l34k1ng_c8ffc55f5e}
-  const unsigned char FLAG_THREE[] = {0xe8, 0xfa, 0xec, 0x1f, 0x1d, 0xd4, 0x19, 0x16, 0x03, 0x06, 0xd5, 0xf2, 0x03, 0xd5, 0x17, 0x03, 0xf0, 0xd7, 0xd8, 0x0f, 0xd5, 0xf2, 0x0b, 0x03, 0x07, 0xdc, 0x0a, 0x0a, 0x07, 0xd9, 0xd9, 0x0a, 0xd9, 0x09, 0x01, 0xa4};
-
-  volatile char buffer[64];
-
-  Solve_Flag_One(FLAG_ONE, buffer);
-  Solve_Flag_Two(FLAG_TWO, sizeof(FLAG_TWO), buffer);
-  Solve_Flag_Three(FLAG_THREE, sizeof(FLAG_THREE), buffer);
+  Lab_SWD_Solve_Flag_One(LAB_SWD_FLAG_ONE, lab_swd_buffer);
+  Lab_SWD_Solve_Flag_Two(LAB_SWD_FLAG_TWO, sizeof(LAB_SWD_FLAG_TWO), lab_swd_buffer);
+  Lab_SWD_Solve_Flag_Three(LAB_SWD_FLAG_THREE, sizeof(LAB_SWD_FLAG_THREE), lab_swd_buffer);
 }
 
 void Lab_SWD_Loop(void) {
--- firmware/tests/unit/src/test_lab_swd.c
+++ firmware/tests/unit/src/test_lab_swd.c
@@ -10,25 +10,25 @@ void setUp(void) {
 
 void tearDown(void) {}
 
-void test_Solve_Flag_One_should_copy(void) {
+void test_Lab_SWD_Solve_Flag_One_should_copy(void) {
   const char mock_flag_one[] = "DVH{m0ck_fl4g_0n3_ffffffffff}";
-  Solve_Flag_One(mock_flag_one, buffer);
+  Lab_SWD_Solve_Flag_One(mock_flag_one, buffer);
 
   TEST_ASSERT_EQUAL_STRING(mock_flag_one, (char*)buffer);
 }
 
-void test_Solve_Flag_Two_should_decrypt(void) {
+void test_Lab_SWD_Solve_Flag_Two_should_decrypt(void) {
   const unsigned char mock_flag_two_encrypted[] = {0xef, 0xfd, 0xe3, 0xd0, 0xc6, 0x9b, 0xc8, 0xc0, 0xf4, 0xcd, 0xc7, 0x9f, 0xcc, 0xf4, 0xdf, 0xdc, 0x9b, 0xf4, 0xcd, 0xcd, 0xcd, 0xcd, 0xcd, 0xcd, 0xcd, 0xcd, 0xcd, 0xcd, 0xd6};
   const char mock_flag_two_plain[] = "DVH{m0ck_fl4g_tw0_ffffffffff}";
-  Solve_Flag_Two(mock_flag_two_encrypted, sizeof(mock_flag_two_encrypted), buffer);
+  Lab_SWD_Solve_Flag_Two(mock_flag_two_encrypted, sizeof(mock_flag_two_encrypted), buffer);
 
   TEST_ASSERT_EQUAL_STRING(mock_flag_two_plain, (char*)buffer);
 }
 
-void test_Solve_Flag_Three_should_decrypt(void) {
+void test_Lab_SWD_Solve_Flag_Three_should_decrypt(void) {
   const unsigned char mock_flag_three_encrypted[] = {0xe8, 0xfa, 0xec, 0x1f, 0xf1, 0xd4, 0x07, 0x0f, 0x03, 0x0a, 0xf0, 0xd8, 0x0b, 0x03, 0x18, 0x0c, 0x16, 0xd7, 0xd7, 0x03, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x01};
   const char mock_flag_three_plain[] = "DVH{m0ck_fl4g_thr33_ffffffffff}";
-  Solve_Flag_Three(mock_flag_three_encrypted, sizeof(mock_flag_three_encrypted), buffer);
+  Lab_SWD_Solve_Flag_Three(mock_flag_three_encrypted, sizeof(mock_flag_three_encrypted), buffer);
 
   TEST_ASSERT_EQUAL_STRING(mock_flag_three_plain, (char*)buffer);
 }
@@ -36,9 +36,9 @@ void test_Solve_Flag_Three_should_decrypt(void) {
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_Solve_Flag_One_should_copy);
-  RUN_TEST(test_Solve_Flag_Two_should_decrypt);
-  RUN_TEST(test_Solve_Flag_Three_should_decrypt);
+  RUN_TEST(test_Lab_SWD_Solve_Flag_One_should_copy);
+  RUN_TEST(test_Lab_SWD_Solve_Flag_Two_should_decrypt);
+  RUN_TEST(test_Lab_SWD_Solve_Flag_Three_should_decrypt);
 
   return UNITY_END();
 }
