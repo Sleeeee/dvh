7854344c074803025ff2981224174892d6355e2e
Sleeeee <jstasjustin@gmail.com>
2026-01-22 13:24:28 +0100

    DVH #11 Implement basic UART shell behavior

--- firmware/shared/src/utils_uart.c
+++ firmware/shared/src/utils_uart.c
@@ -1,4 +1,5 @@
 #include "utils_uart.h"
+#include <string.h>
 
 extern UART_HandleTypeDef huart1;
 
@@ -7,6 +8,39 @@ void Utils_UART_ReceiveEnter(void) {
   while (1) {
     if (HAL_UART_Receive(&huart1, &rx, 1, 100) == HAL_OK) {
       if (rx == '\r' || rx == '\n') return;
+    }
+  }
+}
+
+void Utils_UART_Readline(char* buffer, uint16_t max_length) {
+  uint8_t rx;
+  uint16_t idx = 0;
+
+  memset(buffer, 0, max_length);
+
+  while (1) {
+    // Receive characters one by one
+    if (HAL_UART_Receive(&huart1, &rx, 1, 100) == HAL_OK) {
+      if (rx == '\r' || rx == '\n') {
+        // Handle [ENTER] : send carriage return + newline and exit function
+        HAL_UART_Transmit(&huart1, (uint8_t*)"\r\n", 2, 10);
+        buffer[idx] = '\0';
+        return;
+
+      } else if (rx == '\b' || rx == 0x7f) {
+        // Handle [BACKSPACE] : delete character (effectively move back and overwrite the character with a whitespace)
+        if (idx > 0) {
+          HAL_UART_Transmit(&huart1, (uint8_t*)"\b \b", 3, 10);
+          idx--;
+        }
+
+      } else if (idx < (max_length - 1)) {
+        if (rx >= ' ' && rx <= '~') {
+          // Accept ASCII-printable character (between ' ' / 0x20 and '~' / 0x7e)
+          HAL_UART_Transmit(&huart1, &rx, 1, 10);
+          buffer[idx++] = rx;
+        }
+      }
     }
   }
 }
