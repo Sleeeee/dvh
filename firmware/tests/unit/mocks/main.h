5f2d9ea35fb272d2f68dfa4ac68cdfc6494346c9
Sleeeee <jstasjustin@gmail.com>
2026-05-03 17:20:08 +0200

    DVH #22 Adapt mock GPIO definitions to v1 board

--- firmware/tests/unit/mocks/main.h
+++ firmware/tests/unit/mocks/main.h
@@ -3,9 +3,20 @@
 
 #include "stm32f1xx_hal.h"
 
-#define GPIOA ((GPIO_TypeDef*)0)
+#define GPIOZ ((GPIO_TypeDef*)0)
 
-#define DOOR_IN_Pin 0
-#define DOOR_IN_GPIO_Port ((GPIO_TypeDef*)0)
+#define DOOR_OUT_Pin 0
+#define DOOR_OUT_GPIO_Port GPIOZ
+
+#define LAB_RST_Pin 0
+#define LAB_RST_GPIO_Port GPIOZ
+#define LAB_S0_Pin 0
+#define LAB_S0_GPIO_Port GPIOZ
+#define LAB_S1_Pin 0
+#define LAB_S1_GPIO_Port GPIOZ
+#define LAB_S2_Pin 0
+#define LAB_S2_GPIO_Port GPIOZ
+#define LAB_S3_Pin 0
+#define LAB_S3_GPIO_Port GPIOZ
 
 #endif
