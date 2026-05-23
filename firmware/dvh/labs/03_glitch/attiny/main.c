409701dc12c6e45e4a0a031f2afe2be466edc468
Sleeeee <jstasjustin@gmail.com>
2026-05-23 10:55:45 +0200

    DVH #13 Add final adjustments to glitching UI/UX

--- firmware/dvh/labs/03_glitch/attiny/main.c
+++ firmware/dvh/labs/03_glitch/attiny/main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define TX_PIN PB3 
+#define TX_PIN PB3
 #define TRIG_PIN PB4
 #define BIT_DELAY 715
 
@@ -25,12 +25,6 @@ void uart_tx_byte(uint8_t data) {
 
   PORTB |= (1 << TX_PIN); // Stop bit (HIGH)
   _delay_us(BIT_DELAY);
-}
-
-void uart_print(const char* str) {
-  while (*str) {
-    uart_tx_byte(*str++);
-  }
 }
 
 int main(void) {
@@ -58,45 +52,34 @@ int main(void) {
   CLKPR = (1 << CLKPCE);
   CLKPR = (1 << CLKPS2) | (1 << CLKPS1);
 
-  DDRB |= (1 << TX_PIN) | (1 << TRIG_PIN); 
-  PORTB |= (1 << TX_PIN); 
-  PORTB &= ~(1 << TRIG_PIN); 
+  DDRB |= (1 << TX_PIN) | (1 << TRIG_PIN);
+  PORTB |= (1 << TX_PIN);
+  PORTB &= ~(1 << TRIG_PIN);
   _delay_ms(100);
 
   if (is_glitch && !(reset_cause & (1 << EXTRF))) {
-    uart_tx_byte(0xca); // Magic byte
+    uart_tx_byte(0xca); // First flag / Crashed
   }
 
   while(1) {
-    uart_print("[UART] Verifying ATTiny signature");
-    _delay_ms(500);
-    uart_tx_byte('.');
-    _delay_ms(500);
-    uart_tx_byte('.');
-    _delay_ms(500);
-    uart_tx_byte('.');
-    _delay_ms(20); 
-
     PORTB |= (1 << TRIG_PIN);
-    _delay_us(200);
+    _delay_us(20);
 
-    // If any loop iteration gets skipped, count will be wrong
     volatile uint16_t count = 0;
     for (volatile uint16_t i = 0; i < 500; i++) {
         count++;
     }
 
-    PORTB &= ~(1 << TRIG_PIN); // Lower TRIG
+    PORTB &= ~(1 << TRIG_PIN);
 
     if (count != 500) {
-      uart_tx_byte(0xcb);
+      _delay_ms(10);
+      uart_tx_byte(0xcb); // Second flag
       _delay_ms(10000);
     } else {
-      uart_print("DENIED ");
+      uart_tx_byte(0xc0); // Failed
     }
 
-    _delay_ms(4000);
-    uart_tx_byte('!'); // Clear screen
-    _delay_ms(1000);
+    _delay_ms(2);
   }
 }
--- firmware/dvh/labs/03_glitch/src/lab_glitch.c
+++ firmware/dvh/labs/03_glitch/src/lab_glitch.c
@@ -19,38 +19,66 @@ Lab_StatusTypeDef Lab_Glitch_Init(void) {
 }
 
 void Lab_Glitch_Loop(void) {
+  static bool ui_printed = false;
   static bool browned_out = false;
+
+  static uint32_t last_dot = 0;
+  static uint32_t last_corrupt = 0;
+
   uint8_t c;
-  HAL_StatusTypeDef status = HAL_UART_Receive(&huart2, &c, 1, 10000);
+
+  if (!ui_printed) {
+    Utils_Screen_Fill_Write("[ATTINY] Running integrity checks", UTILS_SCREEN_STANDARD);
+    ui_printed = true;
+  }
+  HAL_StatusTypeDef status = HAL_UART_Receive(&huart2, &c, 1, 100);
 
   if (status == HAL_OK) {
-    if (!browned_out && (char)c == 0xca) {
-      char flag[64];
-      Utils_Secrets_Decrypt(LAB_GLITCH_FLAG_ONE, LAB_GLITCH_FLAG_ONE_LEN, flag, sizeof(flag));
-      Utils_Screen_Fill_Write(flag, UTILS_SCREEN_WARNING);
+    if ((char)c == 0xc0) {
+      if (HAL_GetTick() > (last_dot + 500)) {
+        Utils_Screen_WriteChar('.', UTILS_SCREEN_STANDARD);
+        last_dot = HAL_GetTick();
+      }
+      if (HAL_GetTick() > (last_corrupt + 2500)) {
+        Utils_Screen_Write("CORRUPTED, system will reboot now", UTILS_SCREEN_WARNING);
+        last_corrupt = HAL_GetTick();
+        ui_printed = false;
+        HAL_Delay(1000);
+      }
+    }
 
-      browned_out = true;
-      HAL_Delay(20000);
+    else if ((char)c == 0xca) {
+      Utils_Screen_Write("UNSTABLE, ATTiny crashed", UTILS_SCREEN_WARNING);
+
+      if (!browned_out) {
+        char flag[64];
+        Utils_Secrets_Decrypt(LAB_GLITCH_FLAG_ONE, LAB_GLITCH_FLAG_ONE_LEN, flag, sizeof(flag));
+        Utils_Screen_Write(flag, UTILS_SCREEN_WARNING);
+
+        browned_out = true;
+        HAL_Delay(20000);
+      }
 
     } else if ((char)c == 0xcb) {
+      Utils_Screen_Write("VERIFIED, system is stable", UTILS_SCREEN_STANDARD);
+
       char flag[64];
       Utils_Secrets_Decrypt(LAB_GLITCH_FLAG_TWO, LAB_GLITCH_FLAG_TWO_LEN, flag, sizeof(flag));
-      Utils_Screen_Fill_Write(flag, UTILS_SCREEN_WARNING);
+      Utils_Screen_Write(flag, UTILS_SCREEN_WARNING);
 
       while (1) {
         HAL_Delay(20000); // Lab is over for now
       }
 
-    } else if ((char)c == '!') {
-      Utils_Screen_Fill(UTILS_SCREEN_WARNING);
     } else {
       Utils_Screen_WriteChar((char)c, UTILS_SCREEN_WARNING);
     }
 
   } else if (status == HAL_TIMEOUT) {
-      Utils_Screen_Fill_Write("[CRITICAL] ATTiny UART timeout", UTILS_SCREEN_WARNING);
+    Utils_Screen_Fill_Write("[CRITICAL] ATTiny UART timeout", UTILS_SCREEN_WARNING);
   } else if (status == HAL_ERROR) {
-      Utils_Screen_Fill_Write("[CRITICAL] UART hardware error", UTILS_SCREEN_WARNING);
+    Utils_Screen_Fill_Write("[CRITICAL] UART hardware error", UTILS_SCREEN_WARNING);
+    ui_printed = false;
   }
 }
 
